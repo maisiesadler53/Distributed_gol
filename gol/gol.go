c0867e2a534df05eeae0c72ea76b704073564e86
TBExtent <jedpreist@gmail.com>
2021-10-28 19:41:42 +0100

    added todo to channels bit

--- gol/gol.go
+++ gol/gol.go
@@ -11,6 +11,8 @@ type Params struct {
 // Run starts the processing of Game of Life. It should initialise channels and goroutines.
 func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
 
+	//	TODO: Put the missing channels in here.
+
 	ioCommand := make(chan ioCommand)
 	ioIdle := make(chan bool)
 
